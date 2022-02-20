5d3291469046aca4d5d46ba7b19066665d5fd615
alex-krest <73220512+alex-krest@users.noreply.github.com>
2022-02-20 21:23:30 +0200

    Сборка

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import './css/styles.css';
+import cardItem from './templates/card.html';
+
